f36c59e56e8cbb66770ff245caabb5b65a76f32f
HOSDG\faizal.cahya_aam <faizal.cahyanto@anugrah-argon.com>
2023-10-07 16:53:42 +0700

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + FAIZAL</h1>
+      <h1>Vite + React + Faizal</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
